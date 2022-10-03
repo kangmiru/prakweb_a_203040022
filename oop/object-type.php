539f5b6e1c77e9396c8bf069cb18416695298afa
kangmiru <mirufadz25@gmail.com>
2022-10-03 20:20:15 +0700

    menyelesaikan video 5 (commit ulang karena sebelumnya lupa save )

--- oop/object-type.php
+++ oop/object-type.php
@@ -19,7 +19,7 @@ class Produk{
 }
 
 class CetakInfoProduk {
-    public function cetak( $produk ){
+    public function cetak( Produk $produk ){
         $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
         return $str;
     }
