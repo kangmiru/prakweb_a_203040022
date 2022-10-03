09bceb20e14d8bdbd19592773ce7b651d28e4d68
kangmiru <mirufadz25@gmail.com>
2022-10-03 16:26:01 +0700

    menyelesaikan  video 3 property dan method

--- oop/produk.php
+++ oop/produk.php
@@ -1,11 +1,49 @@
 <?php
 
-//Jualan produk
-//komik
-//game
 class Produk{
+    public $judul = "judul", 
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
 
+    // public function sayHello(){
+    //     return "Hello World";
+    // }
+
+    public function getLabel(){
+        return "$this->penulis,$this->penerbit";
+    }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2->judul);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "shonen jump";
+$produk3->harga = 30000;
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
+echo "<br>";
+echo $produk3->getLabel();
+
+echo "<hr>";
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Niel Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : ". $produk3->getLabel();
+
+echo "<br>";
+
+echo "Game : ". $produk4->getLabel();
