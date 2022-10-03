a1994bf91c5ac551e878e782511297e2c74d0137
kangmiru <mirufadz25@gmail.com>
2022-10-03 13:13:45 +0700

    perbaikan css

--- pw/index.php
+++ pw/index.php
@@ -14,7 +14,7 @@ $buku = query("SELECT * FROM buku");
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="css/index.css">
 
     <style>
         img {
@@ -25,6 +25,10 @@ $buku = query("SELECT * FROM buku");
     <title></title>
   </head>
   <body>
+
+    <div class="container">
+      <h2>Daftar Buku</h2>
+    </div>
   
 
 <div>
