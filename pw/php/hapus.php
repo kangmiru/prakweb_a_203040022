44b578e58ae0a3e5b3730d516a1de82c7f2f61b3
kangmiru <mirufadz25@gmail.com>
2022-10-03 15:19:26 +0700

    selesai memperbaiki fitur delete

--- pw/php/hapus.php
+++ pw/php/hapus.php
@@ -11,7 +11,7 @@ $id = $_GET['id'];
 if (hapus($id) > 0) {
     echo "<script>
             alert('data berhasil dihapus');
-            document.location.href = 'admin.php';
+            document.location.href = '../index.php';
             </script>";
 } else {
     echo "<script>
