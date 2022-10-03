cf4e2b5de4824fa921459f8465737a0935f965df
kangmiru <mirufadz25@gmail.com>
2022-10-03 07:23:29 +0700

    menambahkan fungsi CRUD

--- pw/index.php
+++ pw/index.php
@@ -2,25 +2,49 @@
 require 'php/function.php';
 
 $buku = query("SELECT * FROM buku");
+
 ?>
-<html>
-<div class="container">
-    <h2>Daftar Buku Terpopuler</h2>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/index.css">
+
+    <style>
+        img {
+            height: 150px;
+        }
+    </style>
+
+    <title></title>
+  </head>
+  <body>
+  
+
+<div>
+  <a href="php/tambah.php"><button class="tambah">Tambah data disini!!</button></a>
 </div>
 
 <div class="container">
     <table class="table" border = 1 cellpadding = "10" cellspacing="0">
         <tr>
-            <th>Id</th>
+            <th>No</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
             <th>Judul</th>
             <th>Penulis</th>
             <th>Penerbit</th>
-            <th>Gambar</th>
         </tr>
 
         <?php if(empty($buku)):?>
             <tr>
-                <td colspan="4"><p style="color:red; font-style:italic;">data anggota tidak ditemukan!</p></td>
+                <td colspan="4"><p style="color:red; font-style:italic;">data buku tidak ditemukan!</p></td>
             </tr>
         <?php endif;?>
         
@@ -29,13 +53,30 @@ $buku = query("SELECT * FROM buku");
 
         <tr>
             <td><?= $i++ ;?></td>
-            <td><img src="assets/img/<?= $b['gambar'] ;?>" alt=""></td>
-            <td><a class="detail" href="php/detail1.php?id=<?= $b['id'];?>"><?= $b['judul'] ;?></a></td>
+            <td>
+              <a href="hapus.php?id=<?= $b['id'] ;?>" onclick="return comfirm('apakah anda yakin??;')"><button class="hapus">Hapus</button></a>
+              <a href="ubah.php?id=<?= $b['id'] ;?>"><button class="ubah">Ubah</button></a>
+            </td>
+            <td><img src="img/<?= $b['gambar'] ;?>" alt=""></td>
+            <td><?= $b['judul'] ;?></td>
             <td><?= $b['penulis'];?></td>
-            <td><?= $b['penerbit'];?> </td>
+            <td><?= $b['penerbit'];?></td>
         </tr>
 
         <?php endforeach;?>
     </table>
 </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
---
+++ pw/php/hapus.php
@@ -0,0 +1,21 @@
+<?php
+require 'function.php';
+
+if (!isset($_GET['id'])) {
+    header("location: ../index.php");
+    exit;
+}
+
+$id = $_GET['id'];
+
+if (hapus($id) > 0) {
+    echo "<script>
+            alert('data berhasil dihapus');
+            document.location.href = 'admin.php';
+            </script>";
+} else {
+    echo "<script>
+            alert('data gagal dihapus');
+            </script>";
+}
+?>
