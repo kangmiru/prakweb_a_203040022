f985585819f0d2cb09afce6ee45212aa768da07a
kangmiru <mirufadz25@gmail.com>
2022-10-24 14:38:14 +0700

    mengubah nama folder coba-laravel jadi laravel dan menyelesaikan video 7 post model

---
+++ laravel/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home',[
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Hafadz Hazmirullah",
+        "email" => "203040022@mail.unpas.ac.id",
+        "image" => "hafadz.jpg"
+    ]);
+});
+
+Route::get('/blog', [PostController::class, 'index'] );
+
+// halaman single post
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
