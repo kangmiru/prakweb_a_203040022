d03a7e55b12f4182dd0d7a49732f0194366463cd
kangmiru <mirufadz25@gmail.com>
2022-10-25 19:25:27 +0700

    menyelesaikan video 9 database seeder

--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -4,12 +4,12 @@
     <article>
         <h2>{{ $post->title}}</h2>
 
-        <p>By. Hafadz Hazmirullah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="" class="text-decoration-none"></a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
 
 
