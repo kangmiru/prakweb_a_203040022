506f50540a30559f21b80a9c50ee1c9ac189b0b4
kangmiru <mirufadz25@gmail.com>
2022-10-31 14:34:06 +0700

    menyelesaikan video 12 redesign UI

--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post->title}}</h2>
+
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+        <h1 class="mb-3">{{ $post->title}}</h1>
 
         <p>By. <a href="/author/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
-        {!! $post->body !!}
-    </article>
+        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
 
-    <a href="/blog" class="d-block mt-3">Back to Posts</a>
+        <article class="my-3 fs-6">
+        {!! $post->body !!}
+        </article>
+
+        <a href="/blog" class="d-block mt-3">Back to Posts</a>
+        </div>
+    </div>
+</div>
+        
 @endsection
 
 
